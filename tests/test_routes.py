abe088446a3f3df43f715b5df9545d4e0a12380a
jhaemis-hack <jameskehinde101@gmail.com>
2026-06-28 12:46:16 -0400

    completed ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -209,7 +209,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
 
-
     def test_delete_account(self):
         """It should Delete an Account"""
         account = AccountFactory()
@@ -230,4 +229,8 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(deleteResponse.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
